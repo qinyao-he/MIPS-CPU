ec70dcc7b6d558537bc82c60e9e6c00387422fe4
hqythu <hqythu@gmail.com>
2015-11-26 13:58:37 +0800

    串口读写时序 增加串口读写的时间

--- src/IOBridge.vhd
+++ src/IOBridge.vhd
@@ -89,7 +89,7 @@ begin
 
 	MemoryBusFlag <= not WriteEN when (state=INIT or state=DATA_WRITE) else
 					'1';
-	SerialBusFlag <= not WriteEN when (state=INIT or state=DATA_WRITE) else
+	SerialBusFlag <= not WriteEN when (state=INIT or state=DATA_WRITE or state=DATA_READ) else
 					'1';
 	MemoryDataBus <= DataInput2 when MemoryBusFlag='0' else (others => 'Z');
 	SerialDataBus <= DataInput2(7 downto 0) when SerialBusFlag='0' else (others => 'Z');
@@ -98,7 +98,7 @@ begin
 					"00" & Address2;
 
 	SerialRDN <= not ReadEN when (Address2=x"BF00" and state=DATA_READ) else '1';
-	SerialWRN <= not WriteEN when (Address2=x"BF00" and state=DATA_WRITE) else '1';
+	SerialWRN <= not WriteEN when (Address2=x"BF00" and (state=DATA_WRITE or state=DATA_READ)) else '1';
 
 	BF01 <= "00000000000000" & SerialDATA_READY & (SerialTSRE and SerialTBRE);
 
@@ -111,12 +111,9 @@ begin
 				when INIT =>
 					state <= DATA_WRITE;
 				when DATA_WRITE =>
-					state <= INS_READ;
-				when INS_READ =>
 					state <= DATA_READ;
-					BufferData1 <= MemoryDataBus;
 				when DATA_READ =>
-					state <= INIT;
+					state <= INS_READ;
 					case Address2 is
 						when x"BF00" =>
 							BufferData2 <= "00000000" & SerialDataBus;
@@ -125,6 +122,9 @@ begin
 						when others =>
 							BufferData2 <= MemoryDataBus;
 					end case;
+				when INS_READ =>
+					state <= INIT;
+					BufferData1 <= MemoryDataBus;
 				when others =>
 					state <= INIT;
 			end case;

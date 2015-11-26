ed2cdd3b06358e7939bc969d0eb6c05e92df65e9
hqythu <hqythu@gmail.com>
2015-11-27 02:39:06 +0800

    Flash implementation

--- src/FlashAdapter.vhd
+++ src/FlashAdapter.vhd
@@ -33,7 +33,7 @@ entity FlashAdapter is
 	port (
 		Clock : in std_logic;
 		Reset : in std_logic;
-		Address : in std_logic_vector(22 downto 0);
+		Address : in std_logic_vector(22 downto 1);
 		OutputData : out std_logic_vector(15 downto 0);
 
 		FlashByte : out std_logic;
@@ -44,14 +44,40 @@ entity FlashAdapter is
 		FlashRP : out std_logic;
 
 		FlashAddr : out std_logic_vector(22 downto 0);
-		FlashData : in std_logic_vector(15 downto 0)
+		FlashData : inout std_logic_vector(15 downto 0)
 	);
 end FlashAdapter;
 
 architecture Behavioral of FlashAdapter is
-
+	type STATE_TYPE is (OPSET, DATA_READ);
+	signal state : STATE_TYPE;
 begin
 
+	FlashByte <= '1';
+	FlashVpen <= '1';
+	FlashRP <= '1';
+
+	FlashWE <= '0' when state=OPSET else '1';
+	FlashOE <= '0' when state=DATA_READ else '1';
+	FlashAddr <= "00000000000000000000000" when state=OPSET else
+				Address & '0' when state=DATA_READ;
+	FlashData <= x"00FF" when state=OPSET else (others => 'Z');
+
+	process(Clock, Reset)
+	begin
+		if Reset = '1' then
+			state <= OPSET;
+		elsif rising_edge(Clock) then
+			case state is
+				when OPSET =>
+					state <= DATA_READ;
+				when DATA_READ =>
+					state <= OPSET;
+				when others =>
+					state <= OPSET;
+			end case;
+		end if;
+	end;
 
 end Behavioral;
 

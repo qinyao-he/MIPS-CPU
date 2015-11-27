34e10b42cfcc6bfdc477c551add377c31d5eedac
hqythu <hqythu@gmail.com>
2015-11-28 01:01:58 +0800

    generate new letters project files

--- ipcore_dir/Letters.vhd
+++ ipcore_dir/Letters.vhd
@@ -43,7 +43,7 @@ LIBRARY XilinxCoreLib;
 ENTITY Letters IS
   PORT (
     clka : IN STD_LOGIC;
-    addra : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+    addra : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
     douta : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
   );
 END Letters;
@@ -53,7 +53,7 @@ ARCHITECTURE Letters_a OF Letters IS
 COMPONENT wrapped_Letters
   PORT (
     clka : IN STD_LOGIC;
-    addra : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+    addra : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
     douta : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
   );
 END COMPONENT;
@@ -61,8 +61,8 @@ END COMPONENT;
 -- Configuration specification
   FOR ALL : wrapped_Letters USE ENTITY XilinxCoreLib.blk_mem_gen_v7_3(behavioral)
     GENERIC MAP (
-      c_addra_width => 10,
-      c_addrb_width => 10,
+      c_addra_width => 12,
+      c_addrb_width => 12,
       c_algorithm => 1,
       c_axi_id_width => 4,
       c_axi_slave_type => 0,
@@ -97,8 +97,8 @@ END COMPONENT;
       c_mem_type => 3,
       c_mux_pipeline_stages => 0,
       c_prim_type => 1,
-      c_read_depth_a => 800,
-      c_read_depth_b => 800,
+      c_read_depth_a => 2400,
+      c_read_depth_b => 2400,
       c_read_width_a => 16,
       c_read_width_b => 16,
       c_rst_priority_a => "CE",
@@ -115,8 +115,8 @@ END COMPONENT;
       c_use_softecc => 0,
       c_wea_width => 1,
       c_web_width => 1,
-      c_write_depth_a => 800,
-      c_write_depth_b => 800,
+      c_write_depth_a => 2400,
+      c_write_depth_b => 2400,
       c_write_mode_a => "WRITE_FIRST",
       c_write_mode_b => "WRITE_FIRST",
       c_write_width_a => 16,
